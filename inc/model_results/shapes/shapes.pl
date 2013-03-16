ec016429f08df22bc14d5da3d5678c10a511ead7
Joel Berger <joel.a.berger@gmail.com>
2013-03-16 11:49:47 -0500

    use new Physics::UEMColumn::Pulse defaults to simplify shapes script

--- inc/model_results/shapes/shapes.pl
+++ inc/model_results/shapes/shapes.pl
@@ -9,10 +9,10 @@ use Physics::UEMColumn::Auxiliary ':constants';
 my $n = shift or die "Please specify a number of electrons";
 
 my $length = 0.15; # 15 cm
-my $eta_t = me * qe * (0.5) / 3; # 0.5 eV excess energy
+my $dE = '0.5 eV';
 my $vol = (1e-3)**3;
 
-my $for_xi = gen_xi_series( $n, $vol, $eta_t, $length );
+my $for_xi = gen_xi_series( $n, $vol, $dE, $length );
 
 for my $spec ( [1/3, 'prolate' ], [1.0001, 'sphere'], [3, 'oblate'] ) {
   my ( $xi, $shape ) = @$spec;
@@ -28,7 +28,7 @@ for my $spec ( [1/3, 'prolate' ], [1.0001, 'sphere'], [3, 'oblate'] ) {
 }
 
 sub gen_xi_series {
-  my ($n, $vol, $eta_t, $length) = @_; 
+  my ($n, $vol, $dE, $length) = @_; 
 
   return sub {
     my $xi = shift;
@@ -39,12 +39,9 @@ sub gen_xi_series {
     my $pulse = Pulse->new(
       velocity => '1e8 m/s',
       number   => $n,
-      gamma_t  => 0,
-      gamma_z  => 0,
-      eta_t    => $eta_t,
-      eta_z    => $eta_t / 4,
       sigma_t  => $wt**2 / 2,
       sigma_z  => $wz**2 / 2,
+      excess_photoemission_energy => $dE,
     );
 
     my $column = Column->new(
