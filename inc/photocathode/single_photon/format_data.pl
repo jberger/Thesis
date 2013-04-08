2791c566a45b271a1f2defb16fccb29e8f7cadc3
Joel Berger <joel.a.berger@gmail.com>
2013-04-08 11:59:50 -0500

    added error bars to yield which are a quarter of the background

--- inc/photocathode/single_photon/format_data.pl
+++ inc/photocathode/single_photon/format_data.pl
@@ -2,30 +2,32 @@
 
 use strict;
 use warnings;
+use List::Util 'max';
 
 my $file = 'data';
 my $data = do $file or die "Cannot load $file";
 my $pulse_energy = 0.2;
-my $err = 0.1;
 
 my $output = 'w_plot_data.dat';
 open my $fh, '>', $output or die "Cannot open $output";
 
 local $, = " ";
 
-my $bg;
+my $w_err = 0.1;
+
+my ($bg, $yield_err);
 for my $res (sort {$a->{laser} <=> $b->{laser}} values %$data ) {
 
   # lowest power considered background
   unless ($bg) {
     $bg = $res->{yield};
+    $yield_err = $bg / 4;
     next;
   }
 
-  $res->{yield} -= $bg;
   my $w = $res->{w_av};
-  my $yield = $res->{yield};
+  my $yield = $res->{yield} - $bg;
 
-  print $fh join( " ", $res->{laser} * $pulse_energy, $w, $w*$err, $yield, $yield*$err ) . "\n";  
+  print $fh join( " ", $res->{laser} * $pulse_energy, $w, $w*$w_err, $yield, $yield_err ) . "\n";  
 }
 
